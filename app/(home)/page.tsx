1e3a6d105109d8f1ccd460133fb0c85370d18a4d
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-05-30 15:28:51 -0300

    feat: anime feed and animeItem

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,11 @@ import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 import { format } from "date-fns";
 import ReviewItem from "../_components/reviewItem";
+import { db } from "../_lib/prisma";
+import AnimeItem from "../_components/animeItem";
 
-export default function Home() {
+export default async function Home() {
+  const animes = await db.anime.findMany({});
   return (
     <>
       <Header />
@@ -18,6 +21,16 @@ export default function Home() {
       <div className="px-5 mt-6">
         <h2 className="text-xs uppercase font-bold mb-5">Últimas reviews</h2>
         <ReviewItem />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase font-bold mb-5">
+          Animes Recomendados
+        </h2>
+        <div className="py-3 flex flex-col gap-2">
+          {animes.map((anime) => (
+            <AnimeItem key={anime.id} anime={anime} />
+          ))}
+        </div>
       </div>
     </>
   );
--- app/_components/animeItem.tsx
+++ app/_components/animeItem.tsx
@@ -1,5 +1,7 @@
 import { Anime } from "@prisma/client";
 import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
 
 interface AnimeItemProps {
   anime: Anime;
@@ -7,9 +9,35 @@ interface AnimeItemProps {
 
 const AnimeItem = ({ anime }: AnimeItemProps) => {
   return (
-    <Card>
-      <CardContent className="p-0">
-        <h2>{anime.name}</h2>
+    <Card className="min-w-[400px] max-w-[400px] rounded-none">
+      <CardContent className=" p-0 flex flex-row">
+        <div className="w-1/3 h-full">
+          <Image
+            src={anime.coverURL}
+            alt={anime.name}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="w-full rounded-none overflow-hidden"
+          />
+        </div>
+        <div className="w-2/3 p-4">
+          <div className="flex flex-row gap-2 overflow-hidden uppercase">
+            <Badge className="cursor-pointer" variant={"default"}>
+              {anime.type}
+            </Badge>
+            <Badge className="cursor-pointer" variant={"secondary"}>
+              {anime.studio}
+            </Badge>
+          </div>
+          <div className="flex flex-col mt-2 gap-2">
+            <h2 className="text-xl font-bold text-primary overflow-hidden text-ellipsis text-nowrap">
+              {anime.name}
+            </h2>
+            <p className="text-xs font-thin">{`Autor: ${anime.autor}`}</p>
+            <p className="text-xs font-thin">{`Temporada: ${anime.season}`}</p>
+          </div>
+        </div>
       </CardContent>
     </Card>
   );
